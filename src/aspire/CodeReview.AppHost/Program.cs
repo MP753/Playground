3378c9dbc33250d0a79cc202a7ea79eee185a9c4
MP753 <milena.popovic753@gmail.com>
2025-07-18 20:50:39 +0200

    feat(bff): add backend for frontend service

--- src/aspire/CodeReview.AppHost/Program.cs
+++ src/aspire/CodeReview.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.CodeReview_UsersBFF_Api>("codereview-usersbff-api");
 
+builder.AddProject<Projects.CodeReviewUsersBFF_Api>("codereviewusersbff-api");
+
 builder.Build().Run();
